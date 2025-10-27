b23b4a6dd01d05b8c542a63f4bb3734f94dd15d1
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-10-27 19:36:24 +0700

    Update `UserSchema`

--- src/shared/models/user.model.ts
+++ src/shared/models/user.model.ts
@@ -3,7 +3,7 @@ import * as z from 'zod'
 
 export const UserSchema = z.object({
   id: z.number(),
-  email: z.string().email(),
+  email: z.email(),
   name: z.string().min(1).max(100),
   password: z.string().min(8).max(100),
   phoneNumber: z.string().min(9).max(15),

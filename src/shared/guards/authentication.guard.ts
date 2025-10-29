4c6e4bec6fb6f3b087c8090ec118a8c4e60154b4
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-10-29 16:48:10 +0700

    Refactor `AuthenticationGuard`

--- src/shared/guards/authentication.guard.ts
+++ src/shared/guards/authentication.guard.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common'
+import { CanActivate, ExecutionContext, HttpException, Injectable, UnauthorizedException } from '@nestjs/common'
 import { Reflector } from '@nestjs/core'
 import { AuthType, ConditionGuard } from 'src/shared/constants/auth.constant'
 import { AUTH_TYPE_KEY, AuthTypeDecoratorPayload } from 'src/shared/decorators/auth.decorator'
@@ -29,44 +29,70 @@ export class AuthenticationGuard implements CanActivate {
   }
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const authTypeValue = this.getAuthTypeValue(context)
+
     /**
-     * [1] Lấy metadata từ decorator `@Auth()` (nếu có).
-     *     Reflector sẽ đọc metadata từ cả cấp handler (method) và class (controller).
-     *     Nếu không có, mặc định sẽ yêu cầu xác thực Bearer token và dùng điều kiện AND.
+     * Dựa trên danh sách authTypes lấy từ decorator,
+     * ánh xạ sang các guard tương ứng (Bearer / ApiKey / None / ...)
      */
+    const guards = authTypeValue.authTypes.map((authType) => this.authTypeGuardMap[authType])
+
+    return authTypeValue.options.condition === ConditionGuard.Or
+      ? this.handleOrCondition(guards, context)
+      : this.handleAndCondition(guards, context)
+  }
+
+  /**
+   * Lấy metadata từ decorator `@Auth()` (nếu có).
+   * Reflector sẽ đọc metadata từ cả cấp `handler` (method) và `class` (controller).
+   * Nếu không có, mặc định sẽ yêu cầu xác thực `Bearer token` và dùng điều kiện `AND`.
+   */
+  private getAuthTypeValue(context: ExecutionContext): AuthTypeDecoratorPayload {
     const authTypeValue = this.reflector.getAllAndOverride<AuthTypeDecoratorPayload | undefined>(AUTH_TYPE_KEY, [
       context.getHandler(),
       context.getClass(),
     ]) ?? { authTypes: [AuthType.Bearer], options: { condition: ConditionGuard.And } }
+    return authTypeValue
+  }
 
-    /**
-     * [2️] Dựa trên danh sách authTypes lấy từ decorator, 
-     *     ánh xạ sang các guard tương ứng (Bearer / ApiKey / None / ...)
-     */
-    const guards = authTypeValue.authTypes.map((authType) => this.authTypeGuardMap[authType])
+  /**
+   * Xử lý điều kiện `OR`:
+   * Duyệt từng guard;
+   * Nếu bất kỳ guard nào pass -> cho phép access;
+   * Nếu tất cả guard thất bại -> throw exception cuối cùng
+   */
+  private async handleOrCondition(guards: CanActivate[], context: ExecutionContext) {
+    let finalError: any = null
 
-    let error = new UnauthorizedException()
-
-    if (authTypeValue.options.condition === ConditionGuard.Or) {
-      for (const guard of guards) {
-        const canActive = await Promise.resolve(guard.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
-
+    for (const guard of guards) {
+      try {
+        const canActive = await guard.canActivate(context)
         if (canActive) return true
+      } catch (error) {
+        finalError = error
       }
-      throw error
-    } else {
-      for (const guard of guards) {
-        const canActive = await Promise.resolve(guard.canActivate(context)).catch((err) => {
-          error = err
-          return false
-        })
-
-        if (!canActive) throw error
-      }
-      return true
     }
+
+    if (finalError instanceof HttpException) throw finalError
+    throw new UnauthorizedException()
+  }
+
+  /**
+   * Xử lý điều kiện `AND`:
+   * Duyệt từng guard;
+   * Nếu một guard nào fail → throw exception ngay lập tức;
+   * Nếu tất cả đều pass → cho phép truy cập
+   */
+  private async handleAndCondition(guards: CanActivate[], context: ExecutionContext) {
+    for (const guard of guards) {
+      try {
+        const canActive = await guard.canActivate(context)
+        if (!canActive) throw new UnauthorizedException()
+      } catch (error) {
+        if (error instanceof HttpException) throw error
+        throw new UnauthorizedException()
+      }
+    }
+    return true
   }
 }

a9af5f8f408332ea3761598f5267e34f0764dad4
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-10-30 17:25:42 +0700

    Add file validation

--- src/routes/media/media.controller.ts
+++ src/routes/media/media.controller.ts
@@ -1,11 +1,39 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import {
+  BadRequestException,
+  Controller,
+  FileTypeValidator,
+  MaxFileSizeValidator,
+  ParseFilePipe,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
 
 @Controller('media')
 export class MediaController {
   @Post('images/upload')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadFile(@UploadedFile() file: Express.Multer.File) {
+  @UseInterceptors(
+    FileInterceptor('file', {
+      fileFilter: (req, file, cb) => {
+        const allowedMimeTypes = ['image/jpeg', 'image/png', 'image/gif', 'image/webp']
+        if (!allowedMimeTypes.includes(file.mimetype)) {
+          return cb(new BadRequestException('Only image files are allowed'), false)
+        }
+        cb(null, true)
+      },
+    }),
+  )
+  uploadFile(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [
+          new MaxFileSizeValidator({ maxSize: 3 * 1024 * 1024 }), // 3MB
+        ],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     return {
       message: 'Upload file successfully',
     }

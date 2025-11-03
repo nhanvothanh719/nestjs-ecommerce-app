b081a85a3d2d19dabccab5d76eae61aef8241a2c
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-11-03 16:35:37 +0700

    Add more columns to `Order` and `ProductSKUSnapshot` table

--- src/types.ts
+++ src/types.ts
@@ -1,9 +1,19 @@
 /* eslint-disable @typescript-eslint/no-namespace */
+import { ProductTranslationType } from 'src/shared/models/product-translation.model'
 import { VariantsListType } from 'src/shared/models/product.model'
 
 // Document: https://www.npmjs.com/package/prisma-json-types-generator
 declare global {
   namespace PrismaJson {
     type CustomVariantsListType = VariantsListType
+    type SKUSnapshotProductTranslations = Pick<
+      ProductTranslationType,
+      'id' | 'productId' | 'name' | 'description' | 'languageId'
+    >[]
+    type Receiver = {
+      name: string
+      phoneNumber: string
+      address: string
+    }
   }
 }

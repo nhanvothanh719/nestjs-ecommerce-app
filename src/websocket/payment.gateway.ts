03217a9a16b6e55a6788440563317591bb7c50ec
nhanvothanh719 <nhanvothanh719@gmail.com>
2025-11-05 15:31:42 +0700

    Lifecycle, middleware in websocket

--- src/websocket/payment.gateway.ts
+++ src/websocket/payment.gateway.ts
@@ -1,12 +1,32 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from '@nestjs/websockets'
-import { Server } from 'socket.io'
-
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  OnGatewayInit,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets'
+import { Server, Socket } from 'socket.io'
 
 // MEMO: namespace acts as route: `/payment`
 @WebSocketGateway({ namespace: 'payment' })
+// export class PaymentGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
 export class PaymentGateway {
   @WebSocketServer()
   server: Server
+
+  // afterInit(server: Server) {
+  //   console.log('>>> Websocket server initialized')
+  // }
+
+  // handleConnection(client: Socket, ...args: any[]) {
+  //   console.log('>>> Client connected: ', client.id)
+  // }
+
+  // handleDisconnect(client: Socket) {
+  //   console.log('>>> Client disconnected: ', client.id)
+  // }
 
   @SubscribeMessage('send-money')
   handleEvent(@MessageBody() data: string): string {
--- src/websocket/websocket.adapter.ts
+++ src/websocket/websocket.adapter.ts
@@ -1,5 +1,7 @@
 import { IoAdapter } from '@nestjs/platform-socket.io'
-import { Server, ServerOptions } from 'socket.io'
+import { Server, ServerOptions, Socket } from 'socket.io'
+
+const namespaces = ['/', 'payment', 'chat']
 
 export class CustomWebsocketAdapter extends IoAdapter {
   createIOServer(port: number, options?: ServerOptions) {
@@ -12,6 +14,21 @@ export class CustomWebsocketAdapter extends IoAdapter {
         credentials: true,
       },
     })
+
+    const authMiddleware = (socket: Socket, next: (error?: any) => void) => {
+      console.log(`>>> Client connected: ${socket.id}`)
+
+      socket.on('disconnect', () => {
+        console.log(`>>> Client disconnected: ${socket.id}`)
+      })
+
+      next()
+    }
+
+    namespaces.forEach((namespace) => {
+      server.of(namespace).use(authMiddleware)
+    })
+
     return server
   }
 }
